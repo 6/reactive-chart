cd084c8d11817aeded7e2325e4a36ceffe0164d7
Peter Graham <peterghm@gmail.com>
2014-02-03 20:03:36 -0800

    render some data

--- handsonchart.js
+++ handsonchart.js
@@ -1,6 +1,21 @@
 angular.module('HandsOnChartModule', []).directive('handsonchart', function() {
   return {
     restrict: 'AE',
-    template: '<h1>hello world</h1>'
+    template: '<div class="handsonchart">' +
+                '<div class="handsonchart-spreadsheet"></div>' +
+              '</div>',
+    link: function(scope, el, attr) {
+      $('.handsonchart-spreadsheet').handsontable({
+        data: scope.handsonchart.data,
+        minSpareRows: 1,
+        minRows: 5,
+        minCols: 6,
+        stretchH: 'all',
+        contextMenu: true,
+        manualColumnResize: true,
+        colHeaders: true,
+        rowHeaders: true
+      });
+    }
   };
 });
