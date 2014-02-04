ba686cb82215f2f05648a614e1e0bebe11055796
Peter Graham <peterghm@gmail.com>
2014-02-03 20:14:36 -0800

    add canvas

--- handsonchart.js
+++ handsonchart.js
@@ -3,8 +3,11 @@ angular.module('HandsOnChartModule', []).directive('handsonchart', function() {
     restrict: 'AE',
     template: '<div class="handsonchart">' +
                 '<div class="handsonchart-spreadsheet"></div>' +
+                '<canvas class="handsonchart-visualization"></canvas>' +
               '</div>',
     link: function(scope, el, attr) {
+      var canvas = el[0].querySelector(".handsonchart-visualization");
+      var ctx = canvas.getContext("2d");
       $('.handsonchart-spreadsheet').handsontable({
         data: scope.handsonchart.data,
         minSpareRows: 1,
